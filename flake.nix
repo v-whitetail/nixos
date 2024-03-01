24dc856be17078341c4bae188f732f7c5c1e7db3
v-whitetail <white.tail.millwork@gmail.com>
2024-03-01 15:42:59 +0000

    import test

--- flake.nix
+++ flake.nix
@@ -26,65 +26,6 @@
               useUserPackages = true;
               users.v = { pkgs, ... }: {
 		imports = [ ./import-test.nix ];
-                wayland = {
-                  windowManager.sway = {
-                    enable = true;
-                    config = rec {
-                      modifier = "Mod4";
-                    };
-                  };
-                };
-                programs = {
-                  fzf.enable= true;
-                  waybar.enable= true;
-                  thefuck.enable= true;
-                  home-manager.enable= true;
-                  nushell = { 
-                    enable = true;
-                    shellAliases = {
-                      vim = "nvim ./";
-                      nixrc = "sudo nvim/etc/nixos";
-                    };
-                  };
-                };
-                home = {
-                  username = "v";
-                  homeDirectory = "/home/v";
-                  packages = with pkgs; [
-                    # terminal & tools
-                    gh
-                    git
-                    gitui
-                    neovim
-                    rustup
-                    zellij
-                    nushell
-                    alacritty
-                    # wayland & sway
-                    sway
-                    mako
-                    swayr
-                    bemenu
-                    fuzzel
-                    wayland
-                    i3status
-                    swaylock
-                    swayidle
-                    libnotify
-                    wdisplays
-                    xdg-utils
-                    wl-clipboard
-                    # flavor
-                    swaybg
-                    discord
-                    dolphin
-                    firefox
-                    shotman
-                    discordo
-                    sunpaper
-                    ];
-                  stateVersion = "23.11";
-                };
               };
             };
           }
--- import-test.nix
+++ import-test.nix
@@ -1,5 +1,63 @@
 { config, pkgs, ... }:
 
 {
-
+  wayland = {
+    windowManager.sway = {
+      enable = true;
+      config = rec {
+	modifier = "Mod4";
+      };
+    };
+  };
+  programs = {
+    fzf.enable= true;
+    waybar.enable= true;
+    thefuck.enable= true;
+    home-manager.enable= true;
+    nushell = { 
+      enable = true;
+      shellAliases = {
+	vim = "nvim ./";
+	nixrc = "sudo nvim/etc/nixos";
+      };
+    };
+  };
+  home = {
+    username = "v";
+    homeDirectory = "/home/v";
+    packages = with pkgs; [
+      # terminal & tools
+      gh
+      git
+      gitui
+      neovim
+      rustup
+      zellij
+      nushell
+      alacritty
+      # wayland & sway
+      sway
+      mako
+      swayr
+      bemenu
+      fuzzel
+      wayland
+      i3status
+      swaylock
+      swayidle
+      libnotify
+      wdisplays
+      xdg-utils
+      wl-clipboard
+      # flavor
+      swaybg
+      discord
+      dolphin
+      firefox
+      shotman
+      discordo
+      sunpaper
+    ];
+    stateVersion = "23.11";
+  };
 }
