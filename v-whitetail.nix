7c55ecf8fb8f686cdac3b433f3e87967c68151c1
v-whitetail <white.tail.millwork@gmail.com>
2024-03-07 17:40:23 -0500

    indenting

--- v-whitetail.nix
+++ v-whitetail.nix
@@ -61,43 +61,43 @@ in
       ];
       extraLuaConfig = ''
         vim.cmd([[colorscheme everforest]])
-	vim.opt.nu = true
-	vim.opt.wrap = true
-	vim.opt.backup = false
-	vim.opt.tabstop = 4
-	vim.g.mapleader = " "
-	vim.opt.undodir = os.getenv("HOME").."/.vim/undodir"
-	vim.opt.undofile = true
-	vim.opt.swapfile = false
-	vim.opt.hlsearch = false
-	vim.opt.scrolloff = 8
-	vim.opt.expandtab = true
-	vim.opt.incsearch = true
-	vim.opt.shiftwidth = 4
-	vim.opt.updatetime = 50
-	vim.opt.softtabstop = 4
-	vim.opt.colorcolumn = "80"
-	vim.opt.smartindent = true
-	vim.opt.termguicolors = true
-	vim.opt.relativenumber = true
-	vim.keymap.set("i", "jf", "<Esc>")
-	vim.keymap.set("n", "fu", "<C-R>")
-	vim.keymap.set("n", "<leader>fl", "<C-w>l")
-	vim.keymap.set("n", "<leader>fh", "<C-w>h")
-	vim.keymap.set("n", "<leader>fk", "<C-w>k")
-	vim.keymap.set("n", "<leader>fj", "<C-w>j")
-	vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
-	vim.keymap.set("n", "<leader>term", vim.cmd.terminal)
-	vim.keymap.set("n", "<leader>split", vim.cmd.split)
-	vim.keymap.set("n", "<leader>vsplit", vim.cmd.vsplit)
-	vim.keymap.set("t", "jf", "<C-\\><C-n>", {noremap = true})
-	vim.keymap.set("n", "J", "mzJ`z")
-	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-	vim.keymap.set("n", "<C-d>", "<C-d>zz")
-	vim.keymap.set("n", "<C-u>", "<C-u>zz")
-	vim.keymap.set("n", "<leader>com", "0t,2li<Enter><Esc>V=<Esc>")
-	vim.keymap.set("n", "<leader>per", "0t.a<Enter><Esc>V=q002t.a<Enter><Esc>V=q8@0")
+	    vim.opt.nu = true
+	    vim.opt.wrap = true
+	    vim.opt.backup = false
+	    vim.opt.tabstop = 4
+	    vim.g.mapleader = " "
+	    vim.opt.undodir = os.getenv("HOME").."/.vim/undodir"
+	    vim.opt.undofile = true
+	    vim.opt.swapfile = false
+	    vim.opt.hlsearch = false
+	    vim.opt.scrolloff = 8
+	    vim.opt.expandtab = true
+	    vim.opt.incsearch = true
+	    vim.opt.shiftwidth = 4
+	    vim.opt.updatetime = 50
+	    vim.opt.softtabstop = 4
+	    vim.opt.colorcolumn = "80"
+	    vim.opt.smartindent = true
+	    vim.opt.termguicolors = true
+	    vim.opt.relativenumber = true
+	    vim.keymap.set("i", "jf", "<Esc>")
+	    vim.keymap.set("n", "fu", "<C-R>")
+	    vim.keymap.set("n", "<leader>fl", "<C-w>l")
+	    vim.keymap.set("n", "<leader>fh", "<C-w>h")
+	    vim.keymap.set("n", "<leader>fk", "<C-w>k")
+	    vim.keymap.set("n", "<leader>fj", "<C-w>j")
+	    vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
+	    vim.keymap.set("n", "<leader>term", vim.cmd.terminal)
+	    vim.keymap.set("n", "<leader>split", vim.cmd.split)
+	    vim.keymap.set("n", "<leader>vsplit", vim.cmd.vsplit)
+	    vim.keymap.set("t", "jf", "<C-\\><C-n>", {noremap = true})
+	    vim.keymap.set("n", "J", "mzJ`z")
+	    vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+	    vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+	    vim.keymap.set("n", "<C-d>", "<C-d>zz")
+	    vim.keymap.set("n", "<C-u>", "<C-u>zz")
+	    vim.keymap.set("n", "<leader>com", "0t,2li<Enter><Esc>V=<Esc>")
+	    vim.keymap.set("n", "<leader>per", "0t.a<Enter><Esc>V=q002t.a<Enter><Esc>V=q8@0")
       '';
     };
     fuzzel = {
@@ -229,92 +229,92 @@ in
         terminal = d_term;
         menu     = d_menu;
         gaps = {
-	  left       = 8;
-	  right      = 8;
-	  top        = 8;
-	  bottom     = 8;
-	  inner      = 8;
-	  outer      = 8;
-	  vertical   = 8;
-	  horizontal = 8;
-	};
-	window.titlebar = false;
-	colors = {
-	  background    = "#${palette.base00}";
-	  focused = {
-	    text        = "#${palette.base06}";
-	    border      = "#${palette.base0D}";
-	    indicator   = "#${palette.base0B}";
-	    background  = "#${palette.base00}";
-	    childBorder = "#${palette.base08}";
-	  };
-	  focusedInactive = {
-	    text        = "#${palette.base03}";
-	    border      = "#${palette.base00}";
-	    indicator   = "#${palette.base00}";
-	    background  = "#${palette.base00}";
-	    childBorder = "#${palette.base00}";
-	  };
-	  placeholder = {
-	    text        = "#${palette.base03}";
-	    border      = "#${palette.base00}";
-	    indicator   = "#${palette.base00}";
-	    background  = "#${palette.base00}";
-	    childBorder = "#${palette.base00}";
-	  };
-	  unfocused = {
-	    text        = "#${palette.base03}";
-	    border      = "#${palette.base00}";
-	    indicator   = "#${palette.base00}";
-	    background  = "#${palette.base00}";
-	    childBorder = "#${palette.base00}";
-	  };
-	  urgent = {
-	    text        = "#${palette.base09}";
-	    border      = "#${palette.base0E}";
-	    indicator   = "#${palette.base0E}";
-	    background  = "#${palette.base0E}";
-	    childBorder = "#${palette.base0E}";
-	  };
-	};
-	keybindings = with pkgs; {
-	  "${m_key}+Return" = "exec ${d_term}";
-	  "${m_key}+d" = "exec ${d_menu}";
-	  "${m_key}+q" = "kill";
-	  "${m_key}+b" = "splith";
-	  "${m_key}+v" = "splitv";
-	  "${m_key}+f" = "fullscreen";
-	  "${m_key}+s" = "layout toggle split";
-	  "${m_key}+Shift+space" = "floating toggle";
-	  "${m_key}+${u_key}" = "focus up";
-	  "${m_key}+${d_key}" = "focus down";
-	  "${m_key}+${l_key}" = "focus left";
-	  "${m_key}+${r_key}" = "focus right";
-	  "${m_key}+Shift+${u_key}" = "move up";
-	  "${m_key}+Shift+${d_key}" = "move down";
-	  "${m_key}+Shift+${l_key}" = "move left";
-	  "${m_key}+Shift+${r_key}" = "move right";
-	  "${m_key}+1" = "workspace number 1";
-	  "${m_key}+2" = "workspace number 2";
-	  "${m_key}+3" = "workspace number 3";
-	  "${m_key}+4" = "workspace number 4";
-	  "${m_key}+5" = "workspace number 5";
-	  "${m_key}+6" = "workspace number 6";
-	  "${m_key}+7" = "workspace number 7";
-	  "${m_key}+8" = "workspace number 8";
-	  "${m_key}+9" = "workspace number 9";
-	  "${m_key}+0" = "workspace number 10";
-	  "${m_key}+Shift+1" = "move container to workspace number 1";
-	  "${m_key}+Shift+2" = "move container to workspace number 2";
-	  "${m_key}+Shift+3" = "move container to workspace number 3";
-	  "${m_key}+Shift+4" = "move container to workspace number 4";
-	  "${m_key}+Shift+5" = "move container to workspace number 5";
-	  "${m_key}+Shift+6" = "move container to workspace number 6";
-	  "${m_key}+Shift+7" = "move container to workspace number 7";
-	  "${m_key}+Shift+8" = "move container to workspace number 8";
-	  "${m_key}+Shift+9" = "move container to workspace number 9";
-	  "${m_key}+Shift+0" = "move container to workspace number 10";
-	};
+    	  left       = 8;
+    	  right      = 8;
+    	  top        = 8;
+    	  bottom     = 8;
+    	  inner      = 8;
+    	  outer      = 8;
+    	  vertical   = 8;
+    	  horizontal = 8;
+    	};
+    	window.titlebar = false;
+    	colors = {
+    	  background    = "#${palette.base00}";
+    	  focused = {
+    	    text        = "#${palette.base06}";
+    	    border      = "#${palette.base0D}";
+    	    indicator   = "#${palette.base0B}";
+    	    background  = "#${palette.base00}";
+    	    childBorder = "#${palette.base08}";
+    	  };
+    	  focusedInactive = {
+    	    text        = "#${palette.base03}";
+    	    border      = "#${palette.base00}";
+    	    indicator   = "#${palette.base00}";
+    	    background  = "#${palette.base00}";
+    	    childBorder = "#${palette.base00}";
+    	  };
+    	  placeholder = {
+    	    text        = "#${palette.base03}";
+    	    border      = "#${palette.base00}";
+    	    indicator   = "#${palette.base00}";
+    	    background  = "#${palette.base00}";
+    	    childBorder = "#${palette.base00}";
+    	  };
+    	  unfocused = {
+    	    text        = "#${palette.base03}";
+    	    border      = "#${palette.base00}";
+    	    indicator   = "#${palette.base00}";
+    	    background  = "#${palette.base00}";
+    	    childBorder = "#${palette.base00}";
+    	  };
+    	  urgent = {
+    	    text        = "#${palette.base09}";
+    	    border      = "#${palette.base0E}";
+    	    indicator   = "#${palette.base0E}";
+    	    background  = "#${palette.base0E}";
+    	    childBorder = "#${palette.base0E}";
+    	  };
+    	};
+    	keybindings = with pkgs; {
+    	  "${m_key}+Return" = "exec ${d_term}";
+    	  "${m_key}+d" = "exec ${d_menu}";
+    	  "${m_key}+q" = "kill";
+    	  "${m_key}+b" = "splith";
+    	  "${m_key}+v" = "splitv";
+    	  "${m_key}+f" = "fullscreen";
+    	  "${m_key}+s" = "layout toggle split";
+    	  "${m_key}+Shift+space" = "floating toggle";
+    	  "${m_key}+${u_key}" = "focus up";
+    	  "${m_key}+${d_key}" = "focus down";
+    	  "${m_key}+${l_key}" = "focus left";
+    	  "${m_key}+${r_key}" = "focus right";
+    	  "${m_key}+Shift+${u_key}" = "move up";
+    	  "${m_key}+Shift+${d_key}" = "move down";
+    	  "${m_key}+Shift+${l_key}" = "move left";
+    	  "${m_key}+Shift+${r_key}" = "move right";
+    	  "${m_key}+1" = "workspace number 1";
+    	  "${m_key}+2" = "workspace number 2";
+    	  "${m_key}+3" = "workspace number 3";
+    	  "${m_key}+4" = "workspace number 4";
+    	  "${m_key}+5" = "workspace number 5";
+    	  "${m_key}+6" = "workspace number 6";
+    	  "${m_key}+7" = "workspace number 7";
+    	  "${m_key}+8" = "workspace number 8";
+    	  "${m_key}+9" = "workspace number 9";
+    	  "${m_key}+0" = "workspace number 10";
+    	  "${m_key}+Shift+1" = "move container to workspace number 1";
+    	  "${m_key}+Shift+2" = "move container to workspace number 2";
+    	  "${m_key}+Shift+3" = "move container to workspace number 3";
+    	  "${m_key}+Shift+4" = "move container to workspace number 4";
+    	  "${m_key}+Shift+5" = "move container to workspace number 5";
+    	  "${m_key}+Shift+6" = "move container to workspace number 6";
+    	  "${m_key}+Shift+7" = "move container to workspace number 7";
+    	  "${m_key}+Shift+8" = "move container to workspace number 8";
+    	  "${m_key}+Shift+9" = "move container to workspace number 9";
+    	  "${m_key}+Shift+0" = "move container to workspace number 10";
+    	};
       };
     };
   };
