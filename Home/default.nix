c563296fc12da737be1b006b711408cfdf19a3b4
v-whitetail <white.tail.millwork@gmail.com>
2026-05-29 21:09:44 -0400

    trying vr compat stuff

--- Home/default.nix
+++ Home/default.nix
@@ -85,6 +85,9 @@
       ".config/devshells".source = ./DotFiles/devshells;
     };
     packages = with pkgs; [
+      wivrn #temporary vr stuff
+      avahi
+      #avahi-compat
       devenv
       wl-clipboard
       xdg-utils
